977952c10872f68e1e53e7c4b8c7ccb2669a627e
Telsho <elias.thouant@gmail.com>
2021-03-08 17:26:00 +0100

    first commit

---
+++ AuthJWT/Hubs/CallHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AuthJWT.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AuthJWT.Hubs
+{
+    [Authorize]
+    public class CallHub : Hub
+    {
+        public async Task NewUser(string username)
+        {
+            var userInfo = new UserInfo() { userName = username, connectionId = Context.ConnectionId };
+            await Clients.Others.SendAsync("NewUserArrived", JsonSerializer.Serialize(userInfo));
+        }
+
+        public async Task HelloUser(string userName, string user)
+        {
+            var userInfo = new UserInfo() { userName = userName, connectionId = Context.ConnectionId };
+            await Clients.Client(user).SendAsync("UserSaidHello", JsonSerializer.Serialize(userInfo));            
+        }
+
+        public async Task SendSignal(string signal, string user)
+        {
+            await Clients.Client(user).SendAsync("SendSignal", Context.ConnectionId, signal);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnect", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+
+
+    }
+}
