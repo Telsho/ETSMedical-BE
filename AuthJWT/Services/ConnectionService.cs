0936aedc2204d6877da70200a3b85dd0bf753a1b
Telsho <elias.thouant@gmail.com>
2021-03-12 16:26:24 +0100

    Added logout

---
+++ AuthJWT/Services/ConnectionService.cs
@@ -0,0 +1,41 @@
+﻿using AuthJWT.Models;
+using AuthJWT.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace AuthJWT.Services
+{
+    public class ConnectionService : IConnectionService
+    {
+        public IEnumerable<ApplicationUser> GetUsersByRole(string role)
+        {
+            return Connections.Users.Select(u => u.Value).Where(u => u.Role == role);
+        }
+
+        public void LoginUser(ApplicationUser user)
+        {
+            Connections.Users.GetOrAdd(user.UserName, user);
+        }
+
+        public void LogoutUser(string name)
+        {
+            var value = Connections.Users.Select(u => u.Value).Where(v => v.UserName == name).First();
+            Connections.Users.TryRemove(name, out value);
+        }
+    }
+}
+
+public static class Connections
+{
+    static Connections()
+    {
+        Users = new ConcurrentDictionary<string, ApplicationUser>();
+    }
+
+    public static ConcurrentDictionary<string,ApplicationUser> Users{ get; set; }
+}
---
+++ AuthJWT/Services/Interfaces/IConnectionService.cs
@@ -0,0 +1,15 @@
+﻿using AuthJWT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthJWT.Services.Interfaces
+{
+    public interface IConnectionService
+    {
+        public void LoginUser(ApplicationUser user);
+        public void LogoutUser(string name);
+        public IEnumerable<ApplicationUser> GetUsersByRole(string role);
+    }
+}
